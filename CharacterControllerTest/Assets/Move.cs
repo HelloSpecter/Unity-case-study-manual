66c0e2fac8bc50bbd5e0941476e32fbcb501a67b
HelloSpecter <281141569@qq.com>
2021-04-23 23:05:00 +0800

    1
    1

---
+++ CharacterControllerTest/Assets/Move.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour
+{
+	public float speed = 5.0f;
+	public float jumpSpeed = 8.0f;
+	public float gravity = 20.0f;
+	public float pushPower = 2.0f;
+
+	CharacterController cc;
+	Vector3 moveDir = Vector3.zero;
+
+	void Start ()
+	{
+		cc = GetComponent<CharacterController>();
+	}
+
+	void Update ()
+	{
+		if(cc.isGrounded)
+		{
+			moveDir = new Vector3(Input.GetAxis("Horizontal"), 0,
+								Input.GetAxis("Vertical"));
+			moveDir = transform.TransformDirection(moveDir);
+			moveDir *= speed;
+
+			if(Input.GetButton("Jump"))
+				moveDir.y = jumpSpeed;
+		}
+
+		moveDir.y -= gravity * Time.deltaTime;
+		cc.Move(moveDir * Time.deltaTime);
+	}
+
+	void OnControllerColliderHit(ControllerColliderHit hit)
+	{
+		Rigidbody body = hit.collider.attachedRigidbody;
+		if(body == null || body.isKinematic)
+			return;
+		if(hit.moveDirection.y < -0.3f)
+			return;
+
+		Vector3 pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
+		body.velocity = pushDir * pushPower;
+	}
+}
---
+++ CharacterControllerTest/Assets/SimpleMove.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleMove : MonoBehaviour
+{
+	CharacterController cc;
+	public float speed = 5.0f;
+	Vector3 moveDir = Vector3.zero;
+	void Start ()
+	{
+		cc = GetComponent<CharacterController>();
+	}
+
+	void Update ()
+	{
+		//if(cc.isGrounded)
+		{
+			moveDir = new Vector3(Input.GetAxis("Horizontal"), 0,
+								Input.GetAxis("Vertical"));
+			moveDir = transform.TransformDirection(moveDir);
+			cc.SimpleMove(speed * moveDir);
+		}
+	}
+}
